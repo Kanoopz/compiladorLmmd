69f4b53517d6d6bd37ff643dfeca2955d64aab1b
Carlos Alberto Arroyo Gonzalez <43384993+Kanoopz@users.noreply.github.com>
2023-11-15 16:44:23 -0600

    Update lmmdParser.py
    Actualización del parser con la implementación de los puntos neurálgicos para la generación de código de expresiones aritméticas (con la implementación del cubo semántico para consideraciones semánticas).

--- lmmdParser.py
+++ lmmdParser.py
@@ -4,11 +4,24 @@ from lmmdLexer import tokens
 from varsTable import declaredScopedVars
 scopedDeclaredVars = declaredScopedVars()
 
+from neuralgicPoint import NP
+np = NP()
+
+
+
+#operandsStack = []
+#typesStack = []
+#operatorsStack = []
+#actualAritmeticScope = 'noInitialized'
+
+#temporalsCounter = 0
+#quadruplesDictionary = {}
+
 
 
 def p_programa(t):
     '''
-    programa : PROGRAM ID ';' pn_set_global dec_var def_func impl_main pn_get_vars_table
+    programa : PROGRAM ID ';' pn_set_global dec_var def_func impl_main pn_get_vars_table pn_aritmetic_expressions_print
     '''
 
 def p_dec_var(t):
@@ -48,8 +61,8 @@ def p_dec_var_prime(t):
 
 def p_def_func(t):
     '''
-    def_func : FUNCTION tipos_func ID pn_set_scope '(' tipos ID pn_add_var param_prime ')' func_dec_var '{' est est_prime func_return '}' def_func_prime
-        | FUNCTION tipos_func ID pn_set_scope '(' ')' func_dec_var '{' est est_prime func_return '}' def_func_prime
+    def_func : FUNCTION tipos_func ID pn_set_scope pn_set_aritmetic_expressions_scope '(' tipos ID pn_add_var param_prime ')' func_dec_var '{' est est_prime func_return '}' def_func_prime
+        | FUNCTION tipos_func ID pn_set_scope pn_set_aritmetic_expressions_scope '(' ')' func_dec_var '{' est est_prime func_return '}' def_func_prime
         | null
     '''
 
@@ -83,13 +96,13 @@ def p_func_return(t):
 
 def p_def_func_prime(t):
     '''
-    def_func_prime : FUNCTION tipos_func ID pn_set_scope '(' tipos ID pn_add_var param_prime ')' func_dec_var '{' est est_prime func_return '}' def_func_prime
+    def_func_prime : FUNCTION tipos_func ID pn_set_scope pn_set_aritmetic_expressions_scope '(' tipos ID pn_add_var param_prime ')' func_dec_var '{' est est_prime func_return '}' def_func_prime
         | null
     '''
 
 def p_impl_main(t):
     '''
-    impl_main : MAIN '(' ')' '{' est est_prime '}'
+    impl_main : MAIN pn_set_aritmetic_expressions_scope_global '(' ')' '{' est est_prime '}'
     '''
 
 
@@ -102,6 +115,7 @@ def p_est(t):
         | est_deci
         | est_rep_con
         | est_rep_no_con
+        | null
     '''
 
 def p_est_prime(t):
@@ -118,52 +132,68 @@ def p_est_prime(t):
 
 def p_est_asig(t):
      '''
-     est_asig : var_id '=' hiper_exp ';'
+     est_asig : ID '=' hiper_exp ';'
+        | ID '[' hiper_exp ']' '=' hiper_exp ';'
     '''
-     
-def p_var_id(t):
-     '''
-     var_id : ID
-        | ID '[' hiper_exp ']'
-     '''
      
 def p_hiper_exp(t):
      '''
-     hiper_exp : super_exp
-        | super_exp '&' super_exp
-        | super_exp '|' super_exp
+     hiper_exp : super_exp super_exp_prime
     '''
-     
+
 def p_super_exp(t):
      '''
-     super_exp : exp
-        | exp '<' exp
-        | exp '>' exp
-        | exp IGUAL_QUE exp
-        | exp DIFERENTE_QUE exp
+     super_exp : exp exp_prime
+     '''
+
+def p_super_exp_prime(t):
+     '''
+     super_exp_prime : '&' hiper_exp
+                    | '|' hiper_exp
+                    | null
+     '''
+
+def p_exp_prime(t):
     '''
-     
+    exp_prime : '>' pn_aritmetic_expressions_8 super_exp pn_aritmetic_expressions_9
+              | '<' pn_aritmetic_expressions_8 super_exp pn_aritmetic_expressions_9
+              | IGUAL_QUE pn_aritmetic_expressions_8 super_exp pn_aritmetic_expressions_9
+              | DIFERENTE_QUE pn_aritmetic_expressions_8 super_exp pn_aritmetic_expressions_9
+              | null
+    '''
+
 def p_exp(t):
-     '''
-     exp : term
-        | term '+' term
-        | term '-' term
     '''
-     
+    exp : term term_prime
+    '''
+
+def p_term_prime(t):
+    '''
+    term_prime : pn_aritmetic_expressions_4 '+' pn_aritmetic_expressions_2 exp
+               | pn_aritmetic_expressions_4 '-' pn_aritmetic_expressions_2 exp
+               | null
+    '''
+
 def p_term(t):
-     '''
-     term : factor
-        | factor '*' factor
-        | factor '/' factor
     '''
-     
+    term : factor factor_prime
+    '''
+
+def p_factor_prime(t):
+    '''
+    factor_prime : pn_aritmetic_expressions_5 '*' pn_aritmetic_expressions_3 term
+                 | pn_aritmetic_expressions_5 '/' pn_aritmetic_expressions_3 term
+                 | null
+    '''
+
 def p_factor(t):
      '''
-     factor : var_id
+     factor : ID pn_aritmetic_expressions_1
+        | ID pn_aritmetic_expressions_1 '[' hiper_exp ']'
         | ctes
         | est_llam_func
         | sub_exp
-    '''
+     '''
      
 def p_ctes(t):
      '''
@@ -175,7 +205,7 @@ def p_ctes(t):
      
 def p_sub_exp(t):
      '''
-     sub_exp : '(' factor ')'
+     sub_exp : '(' pn_aritmetic_expressions_6 hiper_exp ')' pn_aritmetic_expressions_7
     '''
      
 def p_est_llam_func(t):
@@ -203,13 +233,15 @@ def p_lectura(t):
      
 def p_llam_lect_param(t):
      '''
-     llam_lect_param : var_id llam_lect_param_prime
+     llam_lect_param : ID llam_lect_param_prime
+        | ID '[' hiper_exp ']' llam_lect_param_prime
         | null
     '''
      
 def p_llam_lect_param_prime(t):
      '''
-     llam_lect_param_prime : ',' var_id llam_lect_param_prime
+     llam_lect_param_prime : ',' ID llam_lect_param_prime
+        | ',' ID '[' hiper_exp ']' llam_lect_param_prime
         | null
     '''
      
@@ -280,7 +312,11 @@ def p_error(t):
 #==============================================================================
 #==============================================================================
 #==============================================================================
-
+#==============================================================================
+#==============================================================================
+#==============================================================================
+#==============================================================================
+#==============================================================================
 
 def p_pn_add_var(t):
      '''
@@ -288,8 +324,6 @@ def p_pn_add_var(t):
      '''
      print(t[-1])
      scopedDeclaredVars.addVar(t[-1])
-        
-     
 
 def p_pn_set_global(t):
      '''
@@ -312,12 +346,131 @@ def p_pn_set_type(t):
      print("Type " + t[-1])
      scopedDeclaredVars.setActualType(t[-1])
 
+
 def p_pn_get_vars_table(t):
      '''
      pn_get_vars_table : null
      '''
      scopedDeclaredVars.getVarTable()
 
+#==============================================================================
+#==============================================================================
+#==============================================================================
+
+def p_pn_set_aritmetic_expressions_scope_global(t):
+     '''
+     pn_set_aritmetic_expressions_scope_global : null
+     '''
+     #actualAritmeticScope = 'Global'
+     #print("actualAritmeticScope: " + actualAritmeticScope + "/////////////////////////////////")
+     np.setActualAritmeticScope('Global')
+
+def p_pn_set_aritmetic_expressions_scope(t):
+     '''
+     pn_set_aritmetic_expressions_scope : null
+     '''
+     #actualAritmeticScope = t[-2]
+     #print("actualAitmeticScope: " + actualAritmeticScope + "/////////////////////////////////")
+     np.setActualAritmeticScope(t[-2])
+
+def p_pn_aritmetic_expressions_1(t):
+     '''
+     pn_aritmetic_expressions_1 : null
+     '''
+     #print("New operand (PN1): " + t[-1])
+
+     actualScope = np.getActualAritmeticScope()
+     #print("Checking var in dictionary:")
+     #print(t[-1])
+     #print(actualScope)
+     varInDictionary = scopedDeclaredVars.checkVar(t[-1], actualScope)
+     
+     #print(varInDictionary)
+     #print("varInDictionary[0]", varInDictionary[0])
+     #print("varInDictionary[1]", varInDictionary[1])
+     #print(" ")
+     
+     if(varInDictionary[0] == True):
+        np.addToOperandsStack(t[-1])
+        np.addToTypesStack(varInDictionary[1])
+
+def p_pn_aritmetic_expressions_2(t):
+     '''
+     pn_aritmetic_expressions_2 : null
+     '''
+     #print("New operator (PN2): " + t[-1])
+     #operatorsStack.append(t[-1])
+     np.addToOperatorsStack(t[-1])
+
+def p_pn_aritmetic_expressions_3(t):
+     '''
+     pn_aritmetic_expressions_3 : null
+     '''
+     #print("New operator (PN3): " + t[-1])
+     #operatorsStack.append(t[-1])
+     np.addToOperatorsStack(t[-1])
+
+def p_pn_aritmetic_expressions_4(t):
+     '''
+     pn_aritmetic_expressions_4 : null
+     '''
+     #print("New operator (PN6): " + t[-1])
+     #operatorsStack.append(t[-1])
+     np.processAdditionOrSubstraction()
+
+def p_pn_aritmetic_expressions_5(t):
+     '''
+     pn_aritmetic_expressions_5 : null
+     '''
+     #print("New operator (PN6): " + t[-1])
+     #operatorsStack.append(t[-1])
+     np.processMultiplicationOrDivision()
+
+def p_pn_aritmetic_expressions_6(t):
+     '''
+     pn_aritmetic_expressions_6 : null
+     '''
+     #print("New operator (PN6): " + t[-1])
+     #operatorsStack.append(t[-1])
+     np.addToOperatorsStack(t[-1])
+
+def p_pn_aritmetic_expressions_7(t):
+     '''
+     pn_aritmetic_expressions_7 : null
+     '''
+     #print("New operator (PN7): " + t[-1])
+     #operatorsStack.append(t[-1])
+     np.processRightParenthesis()
+
+def p_pn_aritmetic_expressions_8(t):
+     '''
+     pn_aritmetic_expressions_8 : null
+     '''
+     #print("New relational operator (PN8): " + t[-1])
+     #operatorsStack.append(t[-1])
+     np.processRelationalOperator()
+     np.addToOperatorsStack(t[-1])
+
+def p_pn_aritmetic_expressions_9(t):
+     '''
+     pn_aritmetic_expressions_9 : null
+     '''
+     #print("New relational operator (PN8): " + t[-1])
+     #operatorsStack.append(t[-1])
+     #np.addToOperatorsStack(t[-1])
+     np.processRelationalOperatorPostExpresion()
+
+
+
+def p_pn_aritmetic_expressions_print(t):
+     '''
+     pn_aritmetic_expressions_print : null
+     '''
+     #print("Stacks:")
+     #print(operandsStack)
+     #print(operatorsStack)
+     #print("temporal", temporalsCounter)
+     np.printStacks()
 
 
 
