012d01a5dae763835b3b72388387daeecb1fcf4f
Carlos Alberto Arroyo Gonzalez <43384993+Kanoopz@users.noreply.github.com>
2023-11-15 16:39:28 -0600

    Add files via upload

---
+++ semanticCube.py
@@ -0,0 +1,53 @@
+class typesMatching:
+
+    def __init__(self):
+        self.cube = {
+            ('+', 'int', 'int')         : 'int',
+            ('+', 'int', 'float')        : 'float',
+            ('+', 'float', 'int')        : 'float',
+            ('+', 'float', 'float')       : 'float',
+            ('-', 'int', 'int')         : 'int',
+            ('-', 'int', 'float')        : 'float',
+            ('-', 'float', 'int')        : 'float',
+            ('-', 'float', 'float')       : 'float',
+            ('*', 'int', 'int')         : 'int',
+            ('*', 'int', 'float')        : 'float',
+            ('*', 'float', 'int')        : 'float',
+            ('*', 'float', 'float')       : 'float',
+            ('/', 'int', 'int')         : 'int',
+            ('/', 'int', 'float')        : 'float',
+            ('/', 'float', 'int')        : 'float',
+            ('/', 'float', 'float')       : 'float',
+            ('&', 'bool', 'bool')       : 'bool',
+            ('|', 'bool', 'bool')       : 'bool',
+            ('>', 'int', 'int')         : 'bool',
+            ('>', 'int', 'float')        : 'bool',
+            ('>', 'float', 'int')        : 'bool',
+            ('>', 'float', 'float')       : 'bool',
+            ('<', 'int', 'int')         : 'bool',
+            ('<', 'int', 'float')        : 'bool',
+            ('<', 'float', 'int')        : 'bool',
+            ('<', 'float', 'float')       : 'bool',
+            ('==', 'int', 'int')        : 'bool',
+            ('==', 'int', 'float')       : 'bool',
+            ('==', 'float', 'int')       : 'bool',
+            ('==', 'float', 'float')      : 'bool',
+            ('==', 'char', 'char')        : 'bool',
+            ('==', 'CTE_LETRERO', 'CTE_LETRERO')  : 'bool',
+            ('==', 'bool', 'bool')      : 'bool',
+            ('!=', 'int', 'int')        : 'bool',
+            ('!=', 'int', 'float')       : 'bool',
+            ('!=', 'float', 'int')       : 'bool',
+            ('!=', 'float', 'float')      : 'bool',
+            ('!=', 'char', 'char')        : 'bool',
+            ('!=', 'CTE_LETRERO', 'CTE_LETRERO')  : 'bool',
+            ('!=', 'bool', 'bool')      : 'bool',
+        }
+
+    def match(self, tuple):
+        type = self.cube.get(tuple, 'ERROR: MISMATCH')
+    
+        if(type != 'ERROR: MISMATCH'):
+            return type
+        elif(type == 'ERROR: MISMATCH'):
+            raise TypeError("ERROR: MISMATCH")
