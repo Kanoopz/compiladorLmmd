bde777fa271943c07c7303dcac1cc08ddc67b4fc
Carlos Alberto Arroyo Gonzalez <43384993+Kanoopz@users.noreply.github.com>
2023-11-23 04:19:04 -0600

    Update lmmdMainExecution.py
    Actualización del archivo ejecutador con implementación de la maquina virtual.

--- lmmdMainExecution.py
+++ lmmdMainExecution.py
@@ -1,4 +1,5 @@
 from lmmdParser import *
+from virtualMachine import *
 
 
 file = open('./tests/fileToUse.lmmd')
@@ -6,3 +7,43 @@ input_str = file.read()
 file.close()
 
 parser.parse(input_str)
+
+print("")
+print("PRINT DESDE EL MAIN EXECUTION:")
+print("")
+
+print("resultingQuadruples:")
+print(resultinQuadruplesDictionary)
+
+print("")
+
+print("constantsAddresses:")
+print(constantsMemoryAddresses)
+
+print("")
+
+constants = constantsMemoryAddresses.get('int')
+constantsTwo = constantsMemoryAddresses.get('float')
+
+print("constantsInt:")
+print(constants)
+print("constantsFloat:")
+print(constantsTwo)
+
+constants.update(constantsTwo)
+
+print("All constants in one dictionary:")
+print(constants)
+
+print("")
+
+print("Number of total generated quadruples:")
+print(len(resultinQuadruplesDictionary))
+
+vm = VM(resultinQuadruplesDictionary, constants, len(resultinQuadruplesDictionary))
+
+print("")
+print("EXECUTION")
+print("")
+
+vm.executeQuaduples()
