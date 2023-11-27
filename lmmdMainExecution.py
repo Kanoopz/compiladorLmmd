cf8341779782b47b3771cb4ac80fad314439ce59
Carlos Alberto Arroyo Gonzalez <43384993+Kanoopz@users.noreply.github.com>
2023-11-26 23:58:59 -0600

    Update lmmdMainExecution.py

--- lmmdMainExecution.py
+++ lmmdMainExecution.py
@@ -24,13 +24,17 @@ print("")
 
 constants = constantsMemoryAddresses.get('int')
 constantsTwo = constantsMemoryAddresses.get('float')
+constantsThree = constantsMemoryAddresses.get('string')
 
 print("constantsInt:")
 print(constants)
 print("constantsFloat:")
 print(constantsTwo)
+print("constantsStringt:")
+print(constantsThree)
 
 constants.update(constantsTwo)
+constants.update(constantsThree)
 
 print("All constants in one dictionary:")
 print(constants)
@@ -40,7 +44,12 @@ print("")
 print("Number of total generated quadruples:")
 print(len(resultinQuadruplesDictionary))
 
-vm = VM(resultinQuadruplesDictionary, constants, len(resultinQuadruplesDictionary))
+print("")
+
+print("Functions dictionary:")
+print(funtionsDictionary)
+
+vm = VM(resultinQuadruplesDictionary, constants, len(resultinQuadruplesDictionary), funtionsDictionary, variablesInformation)
 
 print("")
 print("EXECUTION")
