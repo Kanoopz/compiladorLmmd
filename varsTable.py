ebf148803ea9c63cbd8af875fb520d59fb019f4f
Carlos Alberto Arroyo Gonzalez <43384993+Kanoopz@users.noreply.github.com>
2023-11-27 00:00:10 -0600

    Update varsTable.py

--- varsTable.py
+++ varsTable.py
@@ -1,6 +1,7 @@
 class declaredScopedVars:
     def __init__(self):
         self.declaredVarsScope = {}
+        self.isArray = {}
 
         self.actualScope = 'noInitialized'
         self.actualType = 'noInitialized'
@@ -8,30 +9,24 @@ class declaredScopedVars:
 
     def addVarInScope(self, scope, var, type): 
         scopeInDictonary = self.declaredVarsScope.get(scope, 'notDeclared')
-        
-        print("Searching in dictonary the scope: ", scope)
-        print("Result: ", scopeInDictonary)
-
 
         if(scopeInDictonary == 'notDeclared'):
             self.declaredVarsScope[scope] = {}
-            print("Added new scope: ", self.declaredVarsScope)
 
         
         varInDictonary = self.declaredVarsScope[scope].get(var, 'notDeclared')
 
         if(varInDictonary == 'notDeclared'):
-            print("Var not declared, adding to dictonary.")
             self.declaredVarsScope[scope][var] = type
-            print("Resulting dictonary after adding var: ", self.declaredVarsScope)
         else:
-            print("ERROR: var already declared.")
 
             raise TypeError("ERROR: " + var + " already declared in " + scope + " scope.")
-        print(" ")
 
     def setActualScope(self, scope): 
         self.actualScope = scope
+
+    def getActualScope(self): 
+        return self.actualScope
 
     def setActualType(self, type):
         self.actualType = type
@@ -39,8 +34,71 @@ class declaredScopedVars:
     def addVar(self, varId):
         self.addVarInScope(self.actualScope, varId, self.actualType)
 
+    def addArray(self, arrayId, spaces):
+        print("ActualDictionary:")
+        print(self.isArray)
+        print("actualScope:")
+        print(self.actualScope)
+        print("actualType")
+        print(self.actualType)
+        print(" ")
+
+        scopeInDictonary = self.isArray.get(self.actualScope, 'notDeclared')
+        
+
+
+        if(scopeInDictonary == 'notDeclared'):
+            self.isArray[self.actualScope] = {}
+
+        
+        varInDictonary = self.isArray[self.actualScope].get(arrayId, 'notDeclared')
+
+        if(varInDictonary == 'notDeclared'):
+            self.isArray[self.actualScope][arrayId] = [self.actualType, spaces]
+
+            return self.isArray
+        else:
+            raise TypeError("ERROR: " + arrayId + " already declared in " + self.actualScope + " scope.")
+
+    def checkVar(self, varId, scope):
+         varInDictonary = self.declaredVarsScope[scope].get(varId, 'notDeclaredInScopeTryingOnGlobal')
+
+         if(varInDictonary == 'notDeclaredInScopeTryingOnGlobal'):
+             varInDictonary2 = self.declaredVarsScope['Global'].get(varId, 'notDeclaredInGlobal')
+
+             if(varInDictonary2 == 'notDeclaredInGlobal'):
+                 raise TypeError("ERROR: " + varId + " not declared in " + scope + " scope or in Global.")
+             else:
+                 return True, varInDictonary2
+         else:
+             return True, varInDictonary
+
+    def checkArray(self, arrayId, scope):
+        scopeInDictionary = self.isArray.get(scope, 'scopeNotInDictionary')
+
+        if(scopeInDictionary != 'scopeNotInDictionary'):
+            varInDictonary = self.isArray[scope].get(arrayId, 'notDeclaredInScopeTryingOnGlobal')
+
+            if(varInDictonary == 'notDeclaredInScopeTryingOnGlobal'):
+                varInDictonary2 = self.isArray['Global'].get(arrayId, 'notDeclaredInGlobal')
+
+                if(varInDictonary2 == 'notDeclaredInGlobal'):
+                    raise TypeError("ERROR: " + arrayId + " not declared in " + scope + " scope or in Global.")
+                else:
+                    return True, varInDictonary2
+            else:
+                return True, varInDictonary
+            
+    def getArrayDic(self):
+        return self.isArray
+        
     def getVarTable(self):
         print("methodGetVarTable")
         print(self.declaredVarsScope)
+        print("methodGetArrays")
+        print(self.isArray)
+
+    def getVarTableData(self):
+        return self.declaredVarsScope
 
         
